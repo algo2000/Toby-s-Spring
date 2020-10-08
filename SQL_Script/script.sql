ffdb510d87e121f9a58cbc20a239d0646d8412a8
leo <algo2000@naver.com>
2020-10-08 13:00:59 +0900

    add user sql query script
    테이블 유저의 SQL 스크립트 추가

---
+++ SQL_Script/script.sql
@@ -0,0 +1,12 @@
+create table User
+(
+    Id        varchar(10) not null
+        primary key,
+    Name      varchar(20) not null,
+    Password  varchar(20) not null,
+    Recommend int         not null,
+    Login     int         not null,
+    Level     tinyint     not null
+);
+
+
