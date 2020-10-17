02402ed2f85cff3ad8e4482a8dc3b306996b74e0
leo <algo2000@naver.com>
2020-10-18 00:27:58 +0900

    add springbook471
    471페이지 추가
    스프링 AOP 를 이용하여 다이나믹 프록시를 MathodPointCut을 이용하여 기능을 사용하려는 곳을 지정하여 DI 적용

---
+++ springbook471/src/main/java/springbook/learningtest/factorybean/Message.java
@@ -0,0 +1,21 @@
+package springbook.learningtest.factorybean;
+
+public class Message
+{
+    private String text;
+
+    private Message(String text)
+    {
+        this.text = text;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public static Message newMessage(String text)
+    {
+        return new Message(text);
+    }
+}
