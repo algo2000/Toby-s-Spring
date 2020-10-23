5ea4f8421f9b42bd9b59823c46834237c9c5ecc9
leo <algo2000@naver.com>
2020-10-23 16:29:24 +0900

    add springbook552 springbook552
    테스트 메소드에 롤백 어노테이션을 추가하여 해당 메소드에서는 롤백이 되지 않도록 설정

---
+++ springbook552/src/main/java/springbook/user/service/UserServiceImpl.java
@@ -0,0 +1,105 @@
+package springbook.user.service;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService
+{
+    private UserDao userDao;
+    private MailSender mailSender;
+
+    public void setUserDao(UserDao userDao)
+    {
+        this.userDao = userDao;
+    }
+    public void setMailSender(MailSender mailSender)
+    {
+        this.mailSender = mailSender;
+    }
+
+    public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+    public static final int MIN_RECOMEND_FOR_GOLD = 30;
+
+    @Override
+    public User get(String id)
+    {
+        return userDao.get(id);
+    }
+
+    @Override
+    public List<User> getAll()
+    {
+        return userDao.getAll();
+    }
+
+    @Override
+    public void deleteAll()
+    {
+        userDao.deleteAll();
+    }
+
+    @Override
+    public void update(User user)
+    {
+        userDao.update(user);
+    }
+
+    @Override
+    public void upgradeLevels()
+    {
+        List<User> users = userDao.getAll();
+        for (User user : users)
+        {
+            if (canUpgradeLevel(user))
+            {
+                upgradeLevel(user);
+            }
+        }
+    }
+
+    public void upgradeLevel(User user)
+    {
+        user.upgradeLevel();
+        userDao.update(user);
+        sendUpgradeEMail(user);
+    }
+
+    @Override
+    public void add(User user)
+    {
+        if (user.getLevel() == null)
+        {
+            user.setLevel(Level.BASIC);
+        }
+        userDao.add(user);
+    }
+
+    public boolean canUpgradeLevel(User user)
+    {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel)
+        {
+            case BASIC: return (user.getLogin()>=MIN_LOGCOUNT_FOR_SILVER);
+            case SILVER: return (user.getRecommend()>=MIN_RECOMEND_FOR_GOLD);
+            case GOLD: return false;
+            default:throw new IllegalArgumentException("Unknown Level : "+currentLevel);
+        }
+    }
+
+    private void sendUpgradeEMail(User user)
+    {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("muragon2015@gmail.com");
+        mailMessage.setSubject("upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name());
+
+        this.mailSender.send(mailMessage);
+    }
+}
